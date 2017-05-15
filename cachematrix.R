334ce4d4d983319c316660234e3f6488aa3fee95
Vivian <moon_0214@hotmail.com>
2017-05-15 14:30:16 -0400

    commit changes

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
-
 ## Write a short comment describing this function
+## initiate to null first, and then assign contents, later mimick the makevector code to set setters and getters 
 
 makeCacheMatrix <- function(x = matrix()) {
+  theInverseMatrix <- NULL
+  set <- function(y) {
+    x <<- y
+    theInverseMatrix <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) theInverseMatrix <<- solve
+  getsolve <- function() theInverseMatrix
 
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
 ## Write a short comment describing this function
+## again, revise the example code to invert the matrix and then cache it, but to be inverted, a matrix needs to have non-zero determinant. Also return Null when new data is introduced  
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+   MyMatrix <- x$getsolve()
+  if (!is.null(MyMatrix)) {
+    message("get data")
+    return(MyMatrix)
+  }
+    data <- x$get()
+  if (!is.matrix(data)) {
+    stop("invalid matrix")
+  }
+  if (det(data) == 0) {
+    message("invalid matrix for inversion")
+    x$setsolve(NULL)
+    return(NULL)
+  } 
+    MyMatrix <- solve(data)
+    x$setsolve(MyMatrix)
+    MyMatrix
 }
+
